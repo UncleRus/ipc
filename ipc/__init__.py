3ff0a6dec0b80fef794d440798ed5faa81575ef7
Oleg Golovanov <ogolovanov@rc-online.org>
2017-05-30 12:26:37 +0500

    Правка ipc.__init__.

--- ipc/__init__.py
+++ ipc/__init__.py
@@ -2,9 +2,10 @@
 
 
 from .ipc import Process, SubprocessInterface, Message, Signal
+from .util import channel
 
 
 __version__ = '1.0.0'
 __all__ = (
-    'Process', 'SubprocessInterface', 'Message', 'Signal'
+    'Process', 'SubprocessInterface', 'Message', 'Signal', 'channel'
 )
